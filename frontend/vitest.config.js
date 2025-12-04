73ec347ddb6edf217fcd949e17cabe751cf963bb
Lukas Scheler <Lukas-Scheler@gmx.de>
2025-12-04 22:11:58 +0100

    updated packages

--- frontend/vitest.config.js
+++ frontend/vitest.config.js
@@ -7,7 +7,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/test/setup.js',
     include: [
       'tests/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}',
       'src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'
